a09aa89b9aad3b203dc6bf67b1868c01a2c28792
PoojithaMudupu <poojitha.mudupu98@gmail.com>
2023-03-29 17:17:28 -0500

    modified

--- routes/computation.js
+++ routes/computation.js
@@ -6,10 +6,17 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var ran=Math.random();
-   
-  res.render('computation', { title: 'computation', key1: `${ran,ran} is ${Math.atan2(ran,ran)}`,
-   val2: `${ran,ran} is ${Math.atanh(ran)}`,
-   sol3: `${ran,ran} is ${Math.cbrt(ran)}`});
+   // var ran=search_params.get("ran")
+   if(req.query.ran!=undefined){
+    ran=req.query.ran;
+   }
+  // let num=Math.atan2(ran,ran)
+  res.render('computation', { title: 'computation', 
+ key1: `${ran} is ${Math.atan2(ran,ran)}`,
+   val2: `${ran} is ${Math.atanh(ran)}`,
+   sol3: `${ran} is ${Math.cbrt(ran)}`
+}
+  );
  
 
 });
